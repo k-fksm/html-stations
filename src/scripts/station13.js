b4724dadf9d048910e539506f2f6b8ddb1c0bb68
福島皓平 <kfksm@prrrrrrrrrro.local>
2021-06-04 02:20:52 +0900

    edit station12

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,4 +1,4 @@
-function getData() {
-    // Arrowという文字列を返す処理をアロー関数を入れて書きましょう。
-    return // ここにアロー関数の処理を書く
+export function getData() {
+    str => "Arrow"// Arrowという文字列を返す処理をアロー関数を入れて書きましょう。
+    return str// ここにアロー関数の処理を書く
 }
